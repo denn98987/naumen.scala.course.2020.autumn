40119ad958a475c85876a504c1c6bc795d9c3c00
Dennis <denn98987@gmail.com>
2020-10-23 22:58:25 +0500

    second homework

--- homeworks/homework_2/src/test/scala/Test.scala
+++ homeworks/homework_2/src/test/scala/Test.scala
@@ -8,5 +8,32 @@ object Test extends TestSuite{
             assert(Exercises.divBy3Or7(5, 9) == Seq(6, 7, 9))
             assert(Exercises.divBy3Or7(0, 100) == Seq(0, 3, 6, 7, 9, 12, 14, 15, 18, 21, 24, 27, 28, 30, 33, 35, 36, 39, 42, 45, 48, 49, 51, 54, 56, 57, 60, 63, 66, 69, 70, 72, 75, 77, 78, 81, 84, 87, 90, 91, 93, 96, 98, 99))
         }
+        'test_sumOfDivBy3Or5 - {
+            assert(Exercises.sumOfDivBy3Or5(3, 5) == 8)
+            assert(Exercises.sumOfDivBy3Or5(0, 10) == 33)
+            assert(Exercises.sumOfDivBy3Or5(-5, 15) == 52)
+        }
+        'test_primeFactor - {
+            assert(Exercises.primeFactor(80) == Seq(2, 5))
+            assert(Exercises.primeFactor(132) == Seq(2, 3, 11))
+            assert(Exercises.primeFactor(179) == Seq(179))
+        }
+        'test_sumScalars - {
+            assert(Exercises.sumScalars(Exercises.Vector2D(0, 0), Exercises.Vector2D(1, 0),
+                Exercises.Vector2D(0, 0), Exercises.Vector2D(0, 1)) == 0)
+            assert(Exercises.sumScalars(Exercises.Vector2D(1, 1), Exercises.Vector2D(2, 2),
+                Exercises.Vector2D(1, 1), Exercises.Vector2D(3, 3)) == 10)
+        }
+        'test_sumCosines - {
+            assert(Exercises.sumCosines(Exercises.Vector2D(-1, 0), Exercises.Vector2D(1, 0),
+                Exercises.Vector2D(0, -1), Exercises.Vector2D(0, 2)) == -2)
+        }
+        'test_sortByHeavyweight - {
+            assert(
+                Exercises.sortByHeavyweight(Exercises.balls) == Seq("Tin", "Platinum", "Nickel", "Aluminum", "Titanium",
+                    "Lead", "Sodium", "Uranium", "Gold", "Tungsten", "Zirconium", "Chrome", "Iron", "Copper", "Silver",
+                    "Plutonium", "Cobalt", "Cesium", "Calcium", "Lithium", "Magnesium", "Potassium", "Graphite" )
+            )
+        }
     }
 }
