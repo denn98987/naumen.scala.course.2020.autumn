377c695748a81bb50e403cb0005a06c1f6d034d3
Dennis <denn98987@gmail.com>
2020-10-24 00:00:35 +0500

    third Homework

--- homeworks/homework_3/src/test/scala/homework_3/Test.scala
+++ homeworks/homework_3/src/test/scala/homework_3/Test.scala
@@ -9,5 +9,20 @@ object Test extends TestSuite{
             val trueStr = "правда"
             assert(Exercises.prettyBooleanFormatter1(true) == trueStr)
         }
+        'test_task1 - {
+            assert(Exercises.prettyBooleanFormatter1(12) == "12")
+            assert(Exercises.prettyBooleanFormatter2(false) == "ложь")
+            assert(Exercises.prettyBooleanFormatter3(false) == "ложь")
+        }
+        'test_task2 - {
+            assert(Exercises.max1(Seq(1, 2, 3)) == 3)
+            assert(Exercises.max2(Seq()) == Seq())
+//            assert(Exercises.max3(Seq()) isNaN)
+        }
+        'test_task3 - {
+            assert(Exercises.sum1(1, 2) == 3)
+            assert(Exercises.sum2(1, 3) == 4)
+            assert(Exercises.sum3(1, 4) == 5)
+        }
     }
 }
