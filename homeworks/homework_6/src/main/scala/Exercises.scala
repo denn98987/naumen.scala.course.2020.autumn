1ecb0ab030f85f0130bd968cb4dfddf1f78ec49d
Dennis <denn98987@gmail.com>
2020-12-16 00:08:59 +0500

    sixth homework

--- homeworks/homework_6/src/main/scala/Exercises.scala
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -1,7 +1,10 @@
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+
 object Exercises {
 
 
-  def reverse[T](seq: Seq[T]): Seq[T] = ???
+  def reverse[T](seq: Seq[T]): Seq[T] = seq.reverse
 
   /**
    * https://ru.wikipedia.org/wiki/Числа_Фибоначчи
@@ -9,9 +12,15 @@ object Exercises {
    * @param idx
    * @return
    */
-  def fibonacci4Index(idx: Int): Int = ???
+  def fibonacci4Index(idx: Int): Int = {
+    fibonacci(idx).last
+  }
 
-  def fibonacci(idx: Int): Seq[Int] = ???
+  def fibonacci(idx: Int): Seq[Int] = {
+    val nums = ArrayBuffer(0, 1, 1)
+    for (i <- 3 to idx) nums += (nums(i-1) + nums(i-2))
+    nums
+  }
 
   lazy val MORSE = Map("A" -> ".-", "B" -> "-...", "C" -> "-.-.", "D" -> "-..", "E" -> ".", "F" -> "..-.",
                        "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
@@ -19,9 +28,25 @@ object Exercises {
                        "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
                        "Y" -> "-.--", "Z" -> "--..")
 
-  def morse(text: String): String = ???
+  def morse(text: String): String = text.split("").map(l => MORSE.getOrElse(l.toUpperCase, l)).mkString(" ")
 
 
-  def wordReverse(text: String): String = ???
+  def wordReverse(text: String): String = text.split(" ").map {
+    case word if word.filter(_.isLetter).head.isUpper =>
+      word(word.lastIndexWhere(_.isLetter)).toUpper +
+      word
+        .dropRight(getNonLetterInd(word))
+        .toLowerCase
+        .reverse +
+        word.takeRight( getNonLetterInd(word)-1)
+    case word => word
+      .dropRight(getNonLetterInd(word)-1)
+      .toLowerCase
+      .reverse +
+      word.takeRight(getNonLetterInd(word)-1)
+  }.mkString(" ")
 
+  def getNonLetterInd(word: String) =  {
+    word.length - word.lastIndexWhere(_.isLetter)
+  }
 }
